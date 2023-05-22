54c397e12eee1d909dd90d65bbf71f11dba28c1d
cgccgccgc <126843590+cgccgccgc@users.noreply.github.com>
2023-05-22 20:14:35 +0800

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "男神是康德的御用GPT",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",
