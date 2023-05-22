000f220f0f45297d60429d1cd89bf146b6168748
cgccgccgc <126843590+cgccgccgc@users.noreply.github.com>
2023-05-22 21:31:43 +0800

    sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -120,7 +120,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>男神是康德的御用GPT</div>
-        <div className={styles["sidebar-sub-title"]}>
+        <div className={styles["sidebar-sub-title"]} align="center">
           —人类必胜AI—
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
